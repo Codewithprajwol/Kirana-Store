6c1f4cde47aba24f2e20cbd28c8333e74e3372dc
Prajwol Shrestha <shresthaprajwol161@gmail.com>
2025-01-18 20:10:15 +0545

    fix: added email type in schema

--- Backend/models/user.model.js
+++ Backend/models/user.model.js
@@ -10,6 +10,14 @@ const userSchema = new mongoose.Schema({
         required:[true,"Password is required"],
         minlength:[6,"Password must be at least 6 characters long"]
       },
+      email:{
+        type:String,
+        require:[true,"Email is required"],
+        unique:true,
+        trim:true,
+        lowercase:true,
+      }
+      ,
       cartItems:[{
         quantity:{
             type:number,
