64eb6995d22b680da1343827217d63a9521c1e4e
Prajwol Shrestha <shresthaprajwol161@gmail.com>
2025-01-19 13:20:40 +0545

    finsih:product controller completed

--- Backend/controllers/product.controller.js
+++ Backend/controllers/product.controller.js
@@ -69,3 +69,85 @@ export const createProduct=async(req,res)=>{
         res.status(500).json({error:"Internal Server Error"});
     }
 }
+
+export const getRecommendedProducts=async(req,res)=>{
+    try{
+        const products=await Product.aggregate([
+            {$sample:{size:5}},
+            {$project:{name:1,price:1,image:1,_id:1,description:1}
+        }
+        ])
+        res.json({products})
+
+    }catch(err){
+        console.log('Error in getRecommendedProducts',err.message);
+        res.status(500).json({error:"Internal Server Error"});
+    }
+    
+}
+
+export const deleteProduct=async(req,res)=>{
+    try{
+        const product=await Product.findById(req.params.id);
+        if(!product){
+            return res.status(404).json({error:"product not found"});
+        }
+        if(product.image){
+            const publicId=product.image.split('/').pop().split('.')[0];
+            try{
+                await cloudinary.uploader.destroy(`products/${publicId}`);
+                console.log('image deleted from cloudinary')
+            }catch(err){
+                console.log('errror deleting image from cloudinary',err.message);
+            }
+        }
+        await Product.findByIdAndDelete(req.params.id);
+        res.json({success:true,message:'product deleted successfully'})
+        console.log('product deleted successfully')
+
+    }catch(err){
+        console.log('Error in deleteProduct',err.message);
+        res.status(500).json({error:"Internal Server Error"});
+    }
+}
+
+export const getProductsByCategory=async(req,res)=>{
+    try{
+        const products=await Product.find({category:req.params.category});
+        if(!products){
+            return res.status(404).json({error:"no product found"});
+        }
+        res.json({products})
+    }catch(err){
+        console.log('Error in getProductsByCategory',err.message);
+        res.status(500).json({error:"Internal Server Error"});
+    }
+}
+
+export const toggleFeaturedProducts=async(req,res)=>{
+    try{
+        const product=await Product.findById(req.params.id);
+        if(!product){
+            return res.status(404).json({error:"product not found"});
+        }
+        product.isFeatured=!product.isFeatured;
+        const updatedProduct=await product.save();
+        await updateFeaturedProductsCache();       
+         res.json(updatedProduct);
+
+    }catch(err){
+        console.log('Error in toggleFeaturedProducts',err.message);
+        res.status(500).json({error:"Internal Server Error"});
+    }
+}
+
+async function updateFeaturedProductsCache(){
+    try{
+        let featured_products=await Product.find({isFeatured:true}).lean();
+        await redis.set('featuredProducts',JSON.stringify(featured_products));
+    }
+    catch(err){
+        console.log('Error in updateFeaturedProductsCache',err.message);
+    }
+}
+featuredProducts
--- Backend/routes/product.routes.js
+++ Backend/routes/product.routes.js
@@ -1,16 +1,16 @@
 import express from 'express'
 import { adminRoute, protectRoute } from '../middleware/protectRoute.middleware.js';
-import { createProduct, getAllProducts, getFeaturedProducts } from '../controllers/product.controller.js';
+import { createProduct, deleteProduct, getAllProducts, getFeaturedProducts, getProductsByCategory, getRecommendedProducts } from '../controllers/product.controller.js';
 
 
 const router=express.Router();
 
 router.get('/',protectRoute,adminRoute,getAllProducts);
 router.get('/featured',getFeaturedProducts);
-// router.get('/recommendations',getRecomendedProducts);
-// router.get('/category/:category',getProductsByCategory);
+router.get('/recommendations',getRecommendedProducts);
+router.get('/category/:category',getProductsByCategory);
 
 router.post('/',protectRoute,adminRoute,createProduct);
-// router.patch('/:id',protectRoute,adminRoute,toggleFeaturedProducts);
-// router.delete('/:id',protectRoute,adminRoute,deleteProduct);
+router.patch('/:id',protectRoute,adminRoute,toggleFeaturedProducts);
+router.delete('/:id',protectRoute,adminRoute,deleteProduct);
 export default router;
