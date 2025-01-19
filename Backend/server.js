7420d45d642631ce05f3bea593c11e60a0ed6cb5
Prajwol Shrestha <shresthaprajwol161@gmail.com>
2025-01-19 16:34:48 +0545

    wip: create controllers for coupon

--- Backend/server.js
+++ Backend/server.js
@@ -8,6 +8,7 @@ import { ENV_VARS } from './config/env.config.js'
 import userRoutes from './routes/user.routes.js'
 import productRoutes from './routes/product.routes.js'
 import cartRoutes from './routes/cart.routes.js'
+import couponRoutes from './routes/coupon.routes.js'
 
 const app = express()
 const PORT=ENV_VARS.PORT;
@@ -18,6 +19,7 @@ app.use(cookieParser())
 app.use('/api/auth',userRoutes);
 app.use('/api/products',productRoutes);
 app.use('/api/carts',cartRoutes)
+app.use('/api/coupons',couponRoutes)
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT} `)
